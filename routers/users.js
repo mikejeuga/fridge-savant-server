35d70c4dc284da51eff318a2f81bb34033e5abb3
Noa Farber <noaros88@gmail.com>
2021-06-09 16:25:29 +0100

    post and get requests for users

--- routers/users.js
+++ routers/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('brcyptjs')
-const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator');
 
 
 const User = require('../models/User')
@@ -36,6 +36,8 @@ router.post('/', [
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
 
+        await user.save()
+
         res.send(`${user.name} saved`)
 
     } catch (error) {
@@ -45,3 +47,34 @@ router.post('/', [
 
 });
 
+router.get('/', [
+    check('email', 'Please include a valide email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+    const {email, password } = req.body;
+    try {
+        let user = await User.findOne({email})
+
+        if(!user) {
+            return user.status(400).json({msg: 'Invalid email.'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return user.status(400).json({msg: 'Invalid password.'})
+        }
+
+        res.send(user.id)
+
+    } catch (error) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
+
+})
+module.exports = router
